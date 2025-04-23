7c8cb6d9eae4e771eb580dfb3d701fba0f673922
ElectroDragon777 <viki.mladost@gmail.com>
2025-04-23 17:30:53 +0300

    push 1 zad

--- zad 1/Program.cs
+++ zad 1/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TreeNode root = new TreeNode(1);
+            root.Left = new TreeNode(2);
+            root.Right = new TreeNode(3);
+            root.Left.Left = new TreeNode(4);
+            root.Left.Right = new TreeNode(5);
+            Console.WriteLine("The answer is: " + FindTreeHeight(root));
+        }
+        static int FindTreeHeight(TreeNode root)
+        {
+            if (root == null)
+                return -1;
+
+            int leftHeight = FindTreeHeight(root.Left);
+            int rightHeight = FindTreeHeight(root.Right);
+
+            return Math.Max(leftHeight, rightHeight) + 1;
         }
     }
 }
