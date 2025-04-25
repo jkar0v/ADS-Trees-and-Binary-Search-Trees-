c5a21a8d1306997f1b70b44a8d2dfd0bdb8c98db
ElectroDragon777 <viki.mladost@gmail.com>
2025-04-25 14:58:10 +0300

    zad 4

---
+++ zad 4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace zad_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.Insert(4);
+            tree.Insert(5);
+            tree.Insert(2);
+            tree.Insert(7);
+            tree.Insert(1);
+            
+            List<TreeNode> list = tree.FindMinimum(tree.Root);
+            Console.WriteLine(list[list.Count-1].Value);
+        }
+    }
+}
