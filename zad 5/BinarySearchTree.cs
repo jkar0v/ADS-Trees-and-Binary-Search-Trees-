52ad310c873f1e3b768663cb52004e921a8310d9
Jivko Karov <jikito2007@gmail.com>
2025-04-27 19:55:30 +0300

    zad 5

---
+++ zad 5/BinarySearchTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace zad_4
+{
+    internal class BinarySearchTree
+    {
+        internal TreeNode Root { get; set; }
+        public void Insert(int value)
+        {
+            Root = InsertRecursive(Root, value);
+        }
+        private TreeNode InsertRecursive(TreeNode currentNode, int value)
+        {
+            if (currentNode == null)
+                return new TreeNode(value);
+            if (value < currentNode.Value)
+                currentNode.Left = InsertRecursive(currentNode.Left, value);
+            else if (value > currentNode.Value)
+                currentNode.Right = InsertRecursive(currentNode.Right, value);
+            return currentNode;
+        }
+
+        //List<TreeNode> list = new List<TreeNode>();
+        //internal List<TreeNode> TransferTreeIntoList(TreeNode node)
+        //{
+        //    if (node != null)
+        //    {
+        //        TransferTreeIntoList(node.Left);
+        //        list.Add(node);
+        //        TransferTreeIntoList(node.Right);
+        //    }
+        //    return list;
+        //}
+        int number = -1;
+        internal int FindNumber(TreeNode node, int search)
+        {
+            if (node != null)
+            {
+                if (node.Value <= search)
+                {
+                    number = node.Value;
+                    FindNumber(node.Right, search);
+                }
+                else if (node.Value > search)
+                {
+                    FindNumber(node.Left, search);
+                }
+            }
+            return number;
+        }
+    }
+}
--- zad 5/Program.cs
+++ zad 5/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace zad_5
+﻿using zad_4;
+
+namespace zad_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BinarySearchTree tree = new BinarySearchTree();
+            int[] numbers = { 5, 3, 7, 2, 4, 6, 8 };
+            foreach (int number in numbers)
+            {
+                tree.Insert(number);
+            }
+            Console.WriteLine("Enter a number to find the largest number less than or equal to it in the BST:");
+            int search = int.Parse(Console.ReadLine());
+            int result = tree.FindNumber(tree.Root, search);
+            if (result != -1)
+            {
+                Console.WriteLine($"The largest number less than or equal to {search} in the BST is: {result}");
+            }
+            else
+            {
+                Console.WriteLine($"There is no number less than or equal to {search} in the BST.");
+            }
         }
     }
 }
